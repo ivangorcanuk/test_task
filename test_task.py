4ceeb41ce64d9f8c716f06e42c4f6990cdd83d5c
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-12-25 15:24:10 +0300

    Added a generator

--- test_task.py
+++ test_task.py
@@ -1,6 +1,5 @@
-
+csv_gen = (row for row in open('WikiMatrix.ru.txt', 'r', encoding="utf-8"))
 with open('result.txt', 'w', encoding="utf-8") as output_file:
-    with open('WikiMatrix.ru.txt', 'r', encoding="utf-8") as input_file:
-        for line in input_file:  # Обработка и запись в новый файл
-            if len(line.split(' ')) <= 10:
-                output_file.write(line)
+    for line in csv_gen:  # Обработка и запись в новый файл
+        if len(line.split(' ')) <= 10:
+            output_file.write(line)
